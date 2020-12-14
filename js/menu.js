5da800c0b390a1ba822aa2d5405bd6df4f80cb1d
noerolli <67664822+noerolli@users.noreply.github.com>
2020-12-14 17:00:28 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+var btnMenu = document.getElementById('btnmenu');
+var menu = document.getElementById('menu');
+btnMenu.addEventListener('click', function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+
+
